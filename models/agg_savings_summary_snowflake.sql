0476540adab8f1a315e05f23aa0fc1282856d943
Savannah Chunn-Allnutt <savannah.chunn-allnutt@dbtlabs.com>
2025-12-18 09:42:12 -0500

    adds saving summary model for snowflake

---
+++ models/agg_savings_summary_snowflake.sql
@@ -0,0 +1,29 @@
+{{ config(
+    enabled=dbt_pov_model_cost_calculator.is_enabled('snowflake'),
+    materialized='view',
+    alias='agg_sao_model_savings'
+) }}
+
+-- Savings summary for Snowflake dbt models that were reused
+-- This model creates an aggregate, single row summary of the cost savings from fct_model_queries_snowflake
+-- Run for specific date range with below command, otherwise default will run for the last full 7 days
+-- dbt run --select +agg_savings_summary_snowflake --vars '{"summary_start_date": "2025-12-04", "summary_end_date": "2025-12-11"}'
+
+{% set summary_start_date = var('summary_start_date', (modules.datetime.datetime.now() - modules.datetime.timedelta(days=8)).strftime('%Y-%m-%d')) %}
+{% set summary_start_date = var('summary_start_date', (modules.datetime.datetime.now() - modules.datetime.timedelta(days=1)).strftime('%Y-%m-%d')) %}
+
+select
+--dates
+MIN(reuse_date) as summary_start_date,
+MAX(reuse_date) as summary_end_date,
+ --model counts
+SUM(reuse_count) as weekly_reused_models,
+SUM(execute_count) as weekly_executed_models,
+SUM(reuse_count + execute_count) as weekly_attempted_models,
+ROUND(SUM(reuse_count) / SUM(reuse_count + execute_count), 2) * 100 as weekly_perc_reused_models,
+ --model costs
+SUM(estimated_cost_saved_usd) as weekly_reused_cost_savings,
+SUM(estimated_cost_spent_usd) as weekly_cost_spent,
+ROUND(SUM(estimated_cost_saved_usd) / SUM(estimated_cost_saved_usd + estimated_cost_spent_usd), 2) * 100 as weekly_perc_cost_savings
+from {{ ref('rpt_daily_sao_model_savings_snowflake') }}
+where reuse_date between {{ var('summary_start_date') }} and {{ var('summary_end_date') }}
