7d16077c70b03cd87428f49612bf89884a4825f1
Savannah Chunn-Allnutt <savannah.chunn-allnutt@dbtlabs.com>
2026-01-30 11:13:18 -0500

    updates code comments

--- models/agg_sao_savings_summary_databricks.sql
+++ models/agg_sao_savings_summary_databricks.sql
@@ -7,7 +7,7 @@
 -- Savings summary for Databricks dbt models that were reused
 -- This model creates an aggregate, single row summary of the cost savings from fct_model_queries_databricks
 -- Run for specific date range with below command, otherwise default will run for the last full 7 days
--- dbt run --select +dbt-pov-model-cost-calculator.agg_sao_savings_summary_databricks --vars '{"summary_start_date": "2025-12-04", "summary_end_date": "2025-12-11"}'
+-- dbt run --select package:dbt_pov_model_cost_calculator --vars '{"summary_start_date": "2025-12-04", "summary_end_date": "2025-12-11"}'
 
 {% set summary_start_date = var('summary_start_date', (modules.datetime.datetime.now() - modules.datetime.timedelta(days=8)).strftime('%Y-%m-%d')) %}
 {% set summary_end_date = var('summary_end_date', (modules.datetime.datetime.now() - modules.datetime.timedelta(days=1)).strftime('%Y-%m-%d')) %}
